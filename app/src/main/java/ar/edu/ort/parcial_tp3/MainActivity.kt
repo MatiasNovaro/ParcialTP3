61963bb04616b86a2161d825d4875ad9e9aa34a6
Franco Piatti <41589261@comunidadort.edu.ar>
2025-05-29 22:52:32 -0300

    Agregado de los componentes: globalInput y globalButton Agregado de la pantalla: SplashScreen

--- app/src/main/java/ar/edu/ort/parcial_tp3/MainActivity.kt
+++ app/src/main/java/ar/edu/ort/parcial_tp3/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import ar.edu.ort.parcial_tp3.navigation.Screens
 import ar.edu.ort.parcial_tp3.ui.screens.Home
+import ar.edu.ort.parcial_tp3.ui.screens.onboarding.SplashScreen
 import ar.edu.ort.parcial_tp3.ui.theme.Parcial_TP3Theme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -31,9 +32,10 @@ class MainActivity : ComponentActivity() {
                     NavHost(
                         modifier= Modifier.padding(innerPadding),
                         navController=navController,
-                        startDestination= Screens.Home.screen
+                        startDestination= Screens.SplashScreen.screen
                     ){
                         composable(Screens.Home.screen){ Home(navController)}
+                        composable(Screens.SplashScreen.screen){ SplashScreen(onGetStartedClick = {},navController)}
                     }
                 }
             }
